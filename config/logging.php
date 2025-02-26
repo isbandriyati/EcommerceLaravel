7711a0e4bbf080f512c25ae6e9f7ecdccbb3b886
Didit <isbandriyati6@gmail.com>
2025-02-27 03:06:04 +0700

    Fix logging configuration

--- config/logging.php
+++ config/logging.php
@@ -86,11 +86,7 @@ return [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
             'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
-            'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
-            ],
+            'handler_with' => NullHandler::class,
             'processors' => [PsrLogMessageProcessor::class],
         ],
 
