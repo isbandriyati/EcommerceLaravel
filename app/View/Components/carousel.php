c6ce00c67f98aed596d6a0b8ce36f1d660a18dad
Didit <isbandriyati6@gmail.com>
2025-02-27 04:07:22 +0700

    Fix class and file naming to comply with PSR-4

--- app/View/Components/carousel.php
+++ app/View/Components/carousel.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class carousel extends Component
+class Carousel extends Component
 {
     /**
      * Create a new component instance.
